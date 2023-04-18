d25321994b282d08fd91a599a2ad2b68516743c3
tiesmeys <sebastian.tiesmeyer@charite.de>
2023-04-18 17:10:05 +0200

    Added pip-setup-exe, rescaling function, updated osmFISH-recreate

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='sonar',
+    version='0.1.0',    
+    description='A python package for calculating co-occurrence curves from topographic pixel maps.',
+    url='https://github.com/HiDiHlabs/sonar',
+    author='Sebastian Tiesmeyer',
+    author_email='sebastian.tiesmeyer@bih-charite.de',
+    license='BSD 2-clause',
+    packages=['sonar'],
+    install_requires=[
+    
+    ],
+
+    classifiers=[
+    ],
+)
---
+++ sonar/utils.py
@@ -0,0 +1,16 @@
+# This file provides utils function for sonar co-occurrence calculations.
+#
+#
+
+import torch as t
+
+def rescale_topographic_tensor(topographic_tensor, scaling_factor):
+    """Rescales a topographic tensor by a given factor along the spatial axes [1,2].
+    Args:
+        topographic_tensor (torch.Tensor): Topographic tensor of shape (n_classes, x, y).
+        scaling_factor (float): Scaling factor.
+    Returns:
+        topographic_tensor (torch.Tensor): Rescaled topographic tensor."""
+    
+    return t.nn.functional.interpolate(topographic_tensor.unsqueeze(0), scale_factor=scaling_factor, mode='nearest').squeeze(0)
+
