ab1890b4c5014d9d533f4b262cd5ef44badbe824
tiesmeys <sebastian.tiesmeyer@charite.de>
2023-08-29 14:50:01 +0200

    todo in sonar.py/ plotting.py

---
+++ sonar/plotting.py
@@ -0,0 +1,8 @@
+### plot_co_occurrences(sonar=son)
+### plot_co_occurrences(sonar=son, labels=['acinar_s','alpha','beta'])
+### ploc_co_occurrences(sonar=son, color_label='colors') <- column name in pandas data frame that stores colors
+
+# def plot(son):
+#     co_occurrences = son.co_occurrences
+#     celltypes = son.metadata.index
+#     pixel_counts = son.pixel_counts
